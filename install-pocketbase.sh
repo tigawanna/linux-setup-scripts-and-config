6bbea6ccd1ee864d9037252845805d8c9def3562
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-01 19:41:46 +0300

    more fixes

--- install-pocketbase.sh
+++ install-pocketbase.sh
@@ -5,9 +5,9 @@ pocketbase_url="https://github.com/pocketbase/pocketbase/releases/download/v${ve
 echo "========= downloading pocketbase version ${version} ======="
 wget -q - "$pocketbase_url"
 echo "========= unzipping pocketbase version ${version} ======="
-sudo unzip -q pocketbase_${version}_darwin_amd64.zip -d ./pb
-sudo chmod +x ./pb/pocketbase
-echo "========= pocketbase version ${version} has been downloaded and unzipped successfully! ======="
+sudo unzip -q pocketbase_${version}_darwin_amd64.zip -d /home/ubuntu/pb
+sudo chmod +x ./home/ubuntu/pb/pocketbase
+echo "========= pocketbase version ${version} has been downloaded and unzipped into /home/ubuntu/pb successfully! ======="
 sudo rm -rf pocketbase_${version}_darwin_amd64.zip
 echo "========= setting up a systemd service ======= "
 # setup a systemd service service
